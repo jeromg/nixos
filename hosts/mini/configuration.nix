57122eb1e99e1b91afa435cd70e2d9d6af9f1a94
Jerome Gay <jerome@elixir28.com>
2022-07-24 00:25:55 +0200

    Fixed typo

--- hosts/mini/configuration.nix
+++ hosts/mini/configuration.nix
@@ -8,8 +8,7 @@
         builtins.fetchTarball = {
         url = "https://github.com/nix-community/impermanence/archive/master.tar.gz";
         sha256 = "0000000000000000000000000000000000000000000000000000";
-        };
-      };
+        }
 
   in
 
