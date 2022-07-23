9a85e4e50664f61cd76a6aebdecf199d9b2fa5f4
Jerome Gay <jerome@elixir28.com>
2022-07-23 22:56:18 +0200

    Tuning persistence

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,5 +1,20 @@
 { config, lib, pkgs, user, ... }:
 
+  let 
+    impermanence = builtins.fetchTarBall "https://github.com/nix-community/impermanence/archive/master.tar.gz";
+  in
+  {
+    imports = [ "${impermanence}/nixos.nix" ];
+    environment.persistence."/persist" = {
+        directories = [
+          "/var/lib/bluetooth"
+          "/etc/NetworkManager"
+          "/var/log"
+          "/var/lib"
+        ];
+      };
+   }
+
 {
   home = {
     username = "${user}";
