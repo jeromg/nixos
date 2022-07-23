48c9e6c8fdaeb86cd3f8b1de6cf9bbe72ef90fa9
Jerome Gay <jerome@elixir28.com>
2022-07-23 23:02:23 +0200

    Tuning persistence

--- flake.nix
+++ flake.nix
@@ -1,6 +1,21 @@
 {
   description = "My Flake setup";
 
+  let 
+    impermanence = builtins.fetchTarBall "https://github.com/nix-community/impermanence/archive/master.tar.gz";
+  in
+  {
+    imports = [ "${impermanence}/nixos.nix" ];
+    environment.persistence."/persist" = {
+        directories = [
+          "/var/lib/bluetooth"
+          "/etc/NetworkManager"
+          "/var/log"
+          "/var/lib"
+        ];
+      };
+   }
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
