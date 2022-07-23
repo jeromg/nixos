4d72ff3fc40f8a6474faef9d45eff6c2e0a4d6d2
Jerome Gay <jerome@elixir28.com>
2022-07-23 15:03:02 +0200

    Corrections and minor improvements

--- auto-install.sh
+++ auto-install.sh
@@ -1,30 +1,36 @@
 #!/bin/sh
 
 # Define install disk
-disk=/dev/sda
+disk=/dev/sdb
 
 # Create new GPT
 parted "$disk" -- mklabel gpt
 
 # Create main partition, leaving 512MiB at the beginning 
 # for EFI boot and 8GiB at the end for Swap
-parted "$disk" -- mkpart primary 512MiB -8GiB
+echo "Create main partition"
+parted "$disk" -- mkpart primary 512MiB -12GiB
 
 # Create swap partition
-parted "$disk" -- mkpart primary linux-swap -8GiB 100%
+echo "Create swap partition"
+parted "$disk" -- mkpart primary linux-swap -12GiB 100%
 
 # Create boot partition (ESP and FAT32)
+echo "Create boot partition"
 parted "$disk" -- mkpart ESP fat32 1MiB 512MiB
 parted "$disk" -- set 3 esp on
 
 # Make swap partition
+echo "Create make and activate swap partition"
 mkswap -L swap "${disk}2"
-swapon /dev/sda2
+swapon ${disk}2
 
 # Format boot partition and force UUID to avoid modification of hardware config
+echo "Format boot partition"
 mkfs.fat -F 32 -n EFI "${disk}3"
 
 # Create encrypted main ZFS pool
+echo "Create ZFS main pool"
 zpool create \
   -o ashift=12 \
   -o autotrim=on \
@@ -40,39 +46,47 @@ zpool create \
   -O encryption=aes-256-gcm \
   -O keylocation=prompt \
   -O keyformat=passphrase \
-  rpool /dev/sda1
+  rpool ${disk}1
 
 # Create root bool and make a blank snapshot (for root reset)
+echo "Create root pool and make initial snapshot"
 zfs create -p -o mountpoint=legacy rpool/local/root
 zfs snapshot rpool/local/root@blank
 
 # Mount root pool
+echo "Mount root pool"
 mount -t zfs rpool/local/root /mnt
 
 # Mount boot partition
+echo "Mount boot pool"
 mkdir /mnt/boot
-mount /dev/sda3 /mnt/boot
+mount ${disk}3 /mnt/boot
 
 # Create and mount nix pool
+echo "Create and mount nix pool"
 zfs create -p -o mountpoint=legacy rpool/local/nix
 mkdir /mnt/nix
 mount -t zfs rpool/local/nix /mnt/nix
 
 # Create and mount home pool
+echo "Create and mount home pool"
 zfs create -p -o mountpoint=legacy rpool/safe/home
 mkdir /mnt/home
 mount -t zfs rpool/safe/home /mnt/home
 
 # Create persist pool to allow for root reset at boot time
+echo "Create and mount persist pool"
 zfs create -p -o mountpoint=legacy rpool/safe/persist
 mkdir /mnt/persist
 mount -t zfs rpool/safe/persist /mnt/persist
 
 # Install git and clone repo
+echo "Install git and clone repo"
 nix-env -iA nixos.git
 git clone https://github.com/jeromg/nixos /mnt/etc/nixos
 
 # Install OS
+echo "Install OS"
 cd /mnt/etc/nixos
 nixos-install --flake .#telperion
 
