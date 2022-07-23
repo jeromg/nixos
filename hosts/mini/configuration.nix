e27359e5c5aaa74173e6f28136a8411afa1833df
Jerome Gay <jerome@elixir28.com>
2022-07-24 00:29:13 +0200

    Fixed typo

--- hosts/mini/configuration.nix
+++ hosts/mini/configuration.nix
@@ -9,7 +9,7 @@
           url = "https://github.com/nix-community/impermanence/archive/master.tar.gz";
           sha256 = "0000000000000000000000000000000000000000000000000000";
         };
-    }
+    };
 
   in
 
