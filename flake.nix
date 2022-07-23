a4e5b14713718075fab88f58147f5c47f7433609
Jerome Gay <jerome@elixir28.com>
2022-07-23 23:46:14 +0200

    Tuning persistence

--- flake.nix
+++ flake.nix
@@ -11,20 +11,10 @@
 
   outputs = inputs @ { self, nixpkgs, home-manager, ... }:
   let 
-    impermanence = builtins.fetchTarBall "https://github.com/nix-community/impermanence/archive/master.tar.gz";
     user = "jerome";
     location = "$HOME/.setup";
   in
   {
-    imports = [ "${impermanence}/nixos.nix" ];
-    environment.persistence."/persist" = {
-        directories = [
-          "/var/lib/bluetooth"
-          "/etc/NetworkManager"
-          "/var/log"
-          "/var/lib"
-        ];
-      };
 
       nixosConfigurations = (
         import ./hosts {
@@ -32,5 +22,5 @@
           inherit inputs nixpkgs home-manager user location;
         }
       );
-    };
+    }
 }
