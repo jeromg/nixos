d5d0bffc90aac4ffafa58a15a7b583e091a603eb
Jerome Gay <jerome@elixir28.com>
2022-07-24 00:04:36 +0200

    Fixed typo

--- hosts/mini/configuration.nix
+++ hosts/mini/configuration.nix
@@ -16,6 +16,7 @@
           "/var/log"
           "/var/lib"
         ];
+    };
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
