cdf9ba1cf3cb40ab3e4febd275c8f138b457a164
Jerome <jerome@elixir28.com>
2022-07-30 16:09:32 +0200

    Clean-up NetworkManager config

--- hosts/mini/configuration.nix
+++ hosts/mini/configuration.nix
@@ -41,14 +41,8 @@
   networking = {
     hostName = "melian"; # Define your hostname.
     hostId = "01020304";
-    interfaces.wlp2s0.useDHCP = false;
-    interfaces.wlp2s0.ipv4.addresses = [ {
-      address = "192.168.8.200";
-      prefixLength = 24;
-    } ];
-    defaultGateway = "192.168.8.1";
-    nameservers = [ "208.67.222.222" "208.67.220.220" ];
-    #networkmanager.enable = true;
+    interfaces.wlp2s0.useDHCP = true;
+    networkmanager.enable = true;
   };
 
   # Pick only one of the below networking options.
